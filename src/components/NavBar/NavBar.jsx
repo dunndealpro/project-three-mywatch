5f89486856be0cd0d596da8958d933fb85c125da
dunndealpro <dunn.evan.f@gmail.com>
2023-01-29 09:30:37 -0800

    update Navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom"
 import * as userService from '../../utilities/users-service';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 export default function NavBar({ user, setUser }) {
@@ -10,14 +14,29 @@ export default function NavBar({ user, setUser }) {
         // Update user state in App
         setUser(null);
       }
+
+      let title = "Welcome " + user.name +"!"
+
       
     return (
-        <nav>
-            <Link to="/">Home</Link> &nbsp;&nbsp;
-            <Link to="/mywatch">MyWatch</Link> &nbsp;&nbsp;
-            <Link to="/search">Search</Link>&nbsp;&nbsp;
-            <span>Welcome, {user.name}</span>
-            <Link to="" onClick={handleLogOut}>Log Out</Link>
-        </nav >
+        <Navbar collapseOnSelect expand="lg" className="fs-5 sticky-top bg-primary">
+            <Navbar.Brand className="fs-5 ms-2" href="/">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav.Link href="/mywatch">MyWatch</Nav.Link> &nbsp;&nbsp;
+            <Nav.Link href="/search">Search</Nav.Link>&nbsp;&nbsp;
+            {/* <Nav.Link to="" onClick={handleLogOut}>Log Out</Nav.Link> */}
+            <NavDropdown className="position-absolute end-0 me-3" title={title} id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={handleLogOut} href="">Logout</NavDropdown.Item>
+              
+            </NavDropdown>
+
+
+            </Navbar.Collapse>
+            
+            
+            {/* <Nav.Item className="me-3">Welcome, {user.name}</Nav.Item> */}
+            
+        </Navbar >
     )
 }
