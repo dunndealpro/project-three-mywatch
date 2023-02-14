91301e3f3100d724d7d52d4bda3743373847a68e
dunndealpro <dunn.evan.f@gmail.com>
2023-02-13 16:19:59 -0800

    ready for deployment

--- routes/api/mywatch.js
+++ routes/api/mywatch.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const myWatchCtrl = require('../../controllers/api/mywatch')
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // router.get('/mywatch/', moviesCtrl.getAlreadyWatchedMovies)
 // router.get('/movies/', moviesCtrl.getNextWatchMovies)
-router.post('/mywatch/:id', myWatchCtrl.addToMyWatch)
-router.get('/mywatch', myWatchCtrl.getWatched)
+router.post('/mywatch/:id', ensureLoggedIn,   myWatchCtrl.addToMyWatch)
+router.get('/mywatch', ensureLoggedIn, myWatchCtrl.getWatched)
 router.put('/', myWatchCtrl.getMyWatch)
-router.delete('/:id', myWatchCtrl.deleteFromMyWatch)
-router.post('/:id/comment', myWatchCtrl.addComment)
-router.get('/:id/comment', myWatchCtrl.getComments)
+router.delete('/:id',ensureLoggedIn,  myWatchCtrl.deleteFromMyWatch)
+router.post('/:id/comment', ensureLoggedIn, myWatchCtrl.addComment)
+router.get('/:id/comment', ensureLoggedIn, myWatchCtrl.getComments)
 // router.delete('/movies/', moviesCtrl.deleteFromMyMovies)
 
 module.exports = router;
