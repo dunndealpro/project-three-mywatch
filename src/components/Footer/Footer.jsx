6b2e0f6325a69a23c19a65f1006cd566a095a66e
dunndealpro <dunn.evan.f@gmail.com>
2023-02-15 11:19:39 -0800

    update readme and footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+export default function Footer() {
+
+    return (
+        <>
+        <div className="text-center fs-5 text-dark" style={{ background: 'rgba(0,168,168,1)' }} >
+
+            <a className="text-dark" href="https://www.themoviedb.org/" target="_blank">Powered by TMDB.com</a>
+        </div>
+
+        </>
+    )
+}
